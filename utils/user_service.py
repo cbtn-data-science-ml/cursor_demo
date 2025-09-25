f7e05cfa38794ac9eebb2ec684201e50bc3c4a1d
Jonathan Barrios <git@jonathanbarrios.com>
2025-09-25 09:39:24 -0500

    Update user_service.py

--- utils/user_service.py
+++ utils/user_service.py
@@ -2,7 +2,6 @@ def get_user(x: int) -> dict:
     """Return a user record for the given id."""
     return {"id": x, "name": "Guest"}
 
-Update app.py to call it:
 from utils.user_service import get_user
 
 def main():
